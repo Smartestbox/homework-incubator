fa94296afef5fae10a583faa0e3db839980ad1f0
Igor Olkhov <smartestbox@gmail.com>
2023-02-13 02:31:13 +0100

    Complete 5th homework without styles

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -29,12 +29,13 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
-        if(onChangeChecked) {
-            onChangeChecked(e.currentTarget.checked)
-        }
-        if(onChange) {
-            onChange(e)
-        }
+        // if(onChangeChecked) {
+        //     onChangeChecked(e.currentTarget.checked)
+        // }
+        // if(onChange) {
+        //     onChange(e)
+        // }
+
     }
 
     const finalInputClassName = s.checkbox
