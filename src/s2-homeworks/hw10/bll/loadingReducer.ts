c84192f21e86df789357fdd62188b916b33bc3dd
Igor Olkhov <smartestbox@gmail.com>
2023-04-19 01:13:21 +0200

    Complete HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,19 +1,27 @@
+type LoadingActionType = {
+    type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+
+type InitStateType = {
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitStateType = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         default:
             return state
     }
-}
-
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
