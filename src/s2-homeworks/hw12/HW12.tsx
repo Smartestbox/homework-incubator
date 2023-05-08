692dd8efe1c38c0b5e521f5150b7f4cc5175a4e5
Igor Olkhov <smartestbox@gmail.com>
2023-05-09 00:04:37 +0200

    Delete console.log at HW12.tsx

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -23,7 +23,6 @@ const HW12 = () => {
     // взять ид темы из редакса
     const themeId: number = useSelector<AppStoreType, number>(state => state.theme.themeId)
     const dispatch = useDispatch()
-    console.log(themeId)
 
     const change = (id: number) => { // дописать функцию
         dispatch(changeThemeId(id))
